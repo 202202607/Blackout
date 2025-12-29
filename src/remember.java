cd985432efa1b46487c41829ad7913a73ecc68ff
202202607 <ygh0795@gmail.com>
2025-12-29 16:16:50 +0900

    eee

--- src/remember.java
+++ src/remember.java
@@ -1,6 +1,61 @@
-public class remember {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, commander");
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class remember implements KeyListener {
+
+    public void Mousemove(int x, int y, Robot robot){
+        try{
+            robot.mouseMove(x, y);
+            Thread.sleep(100);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
+
+    public void Mouseclick(Robot robot){
+        try{
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            Thread.sleep(500);
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            Thread.sleep(100);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public int[] Mousexy(){
+        Point point = MouseInfo.getPointerInfo().getLocation();
+        int[] xyArray= new int[2];
+        xyArray[0]=(int)point.getLocation().getX();
+        xyArray[1]=(int)point.getLocation().getY();
+        return xyArray;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        System.out.println("Key pressed: " + KeyEvent.getKeyText(keyCode));
+    }
+
+    public void keyReleased(KeyEvent e) {
+        //int keyCode = e.getKeyCode();
+        //System.out.println("Key released: " + KeyEvent.getKeyText(keyCode));
+    }
+
+    public void keyTyped(KeyEvent e) {
+        // Not used
+    }
+
+    public static void main(String[] args) throws Exception {
+        try{
+            Robot robot=new Robot();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
-/* 왜 안되냐 이거 */
