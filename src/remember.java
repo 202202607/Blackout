903bd19496fedf8ff1bd3e281de0a5ae447a5b90
202202607 <ygh0795@gmail.com>
2025-12-30 15:16:36 +0900

    eeee

--- src/remember.java
+++ src/remember.java
@@ -1,11 +1,45 @@
 import java.awt.Robot;
 import java.awt.event.InputEvent;
+import java.awt.Container;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.JFrame;
 
-public class remember implements KeyListener {
+public class remember extends JFrame {
+
+    public class key implements KeyListener {
+        public void keyPressed(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            System.out.println("Key pressed: " + KeyEvent.getKeyText(keyCode) + " - Key code: " + keyCode);
+            if(keyCode == 27){
+                System.exit(0);
+            }
+        }
+
+        public void keyReleased(KeyEvent e) {
+            //int keyCode = e.getKeyCode();
+            //System.out.println("Key released: " + KeyEvent.getKeyText(keyCode));
+        }
+
+        public void keyTyped(KeyEvent e) {
+            // Not used
+        }
+        
+    }
+
+    public void Make() {
+        setTitle("/ㅡ/");
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //윈도우 창 종료시 프로세스까지 닫기
+
+        Container c = getContentPane();
+        c.addKeyListener(new key());
+        c.setFocusable(true);
+        setSize(200, 200);
+        setVisible(true);
+
+    }
 
     public void Mousemove(int x, int y, Robot robot){
         try{
@@ -35,27 +69,15 @@ public class remember implements KeyListener {
         return xyArray;
     }
 
-    public void keyPressed(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        System.out.println("Key pressed: " + KeyEvent.getKeyText(keyCode));
-    }
-
-    public void keyReleased(KeyEvent e) {
-        //int keyCode = e.getKeyCode();
-        //System.out.println("Key released: " + KeyEvent.getKeyText(keyCode));
-    }
-
-    public void keyTyped(KeyEvent e) {
-        // Not used
-    }
 
     public static void main(String[] args) throws Exception {
         try{
             Robot robot=new Robot();
+            remember wewillbewatching = new remember();
+            wewillbewatching.Make();
         }catch(Exception e){
             e.printStackTrace();
         }
-
     }
 
 }
